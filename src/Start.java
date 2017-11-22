7800e79adfcfa4e7d4b82074de2c6b5d87702819
jesperov <jesperov1@gmail.com>
2017-11-22 12:35:04 +0100

    Initial commit

---
+++ src/Start.java
@@ -0,0 +1,19 @@
+import java.awt.Frame;
+
+public class Start extends Frame{
+
+
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Start().initialize();
+	}
+	
+	
+	
+	void initialize() {	
+		System.out.println("Helle world!");
+	}
+
+}
